410e264fc112eba1b4bf77992f81c241440c0dd3
collinBlessing <nahuriracollinblessing@gmail.com>
2024-06-17 17:00:31 +0300

    registration code added

--- main.py
+++ main.py
@@ -8,40 +8,46 @@ error_color = fg('red')
 success_color = fg('green')
 info_color = fg('blue')
 
+
+# error code
+exit_code = False
+
 # patients data in dictionary format
 patients_data = {}
 
 
-# =========================================> start of program code
-print(info_color + "\n===================================\nWELCOME TO HMS PLEASE REGISTER HERE\n===================================\n")
-# entering patients data on registration
-
-
-try:
-    patient_username = input(normal_color + "Enter your preferred username:\n")
-    patient_first_name = input(normal_color + "Enter your first name:\n")
-    patient_last_name = input(normal_color + "Enter your last name:\n")
-    patient_age = int(input(normal_color + "Enter your age:\n"))
-    patient_address = input(normal_color + "Enter your address:\n")
-    patient_password = getpass(normal_color + "Enter your password:\n")
-    confirm_password = getpass(normal_color + "Confirm password:\n")
-
-    while confirm_password != patient_password:
-        print(error_color + "passwords donot match")
-
-        # enable user to retype the password
-        patient_password = getpass(normal_color + "Enter your new password:\n")
+while exit_code != True:
+    # =========================================> start of program code
+    print(info_color + "\n===================================\nWELCOME TO HMS PLEASE REGISTER HERE\n===================================\n")
+    # entering patients data on registration
+    try:
+        patient_username = input(
+            normal_color + "Enter your preferred username:\n")
+        patient_first_name = input(normal_color + "Enter your first name:\n")
+        patient_last_name = input(normal_color + "Enter your last name:\n")
+        patient_age = int(input(normal_color + "Enter your age:\n"))
+        patient_address = input(normal_color + "Enter your address:\n")
+        patient_password = getpass(normal_color + "Enter your password:\n")
         confirm_password = getpass(normal_color + "Confirm password:\n")
-    else:
-        patients_data = {
-            'username': patient_username,
-            'first_name': patient_first_name,
-            'last_name': patient_last_name,
-            'age': patient_age,
-            'address': patient_address,
-            'password': patient_password
-        }
-        print(success_color + 'Registered successfully')
 
-except:
-    print(error_color + 'Invalid data entry, please enter correct data format')
+        while confirm_password != patient_password:
+            print(error_color + "passwords donot match")
+
+            # enable user to retype the password
+            patient_password = getpass(
+                normal_color + "Enter your new password:\n")
+            confirm_password = getpass(normal_color + "Confirm password:\n")
+        else:
+            patients_data = {
+                'username': patient_username,
+                'first_name': patient_first_name,
+                'last_name': patient_last_name,
+                'age': patient_age,
+                'address': patient_address,
+                'password': patient_password
+            }
+            print(success_color + 'Registered successfully')
+            exit_code = True
+
+    except:
+        print(error_color + 'Invalid data entry, please enter correct data format')
